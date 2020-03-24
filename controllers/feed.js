9e93c5285b49f554b54738d2c0aa9050195894ae
Nedim Kulovac <nedim.kulovac@gmail.com>
2020-03-24 11:08:53 +0100

    moongose instaled, configured and post model created. saving post to db implemented in feed controller

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,14 +1,47 @@
+const {validationResult} = require('express-validator/check');
+
+const Post = require('../models/post');
+
 exports.getPosts = (req, res, next) => {
     res.status(200).json({
-        posts: [{title: 'First post', content: 'This is the first post!'}]
+        posts: [{
+            _id: '1',
+            title: 'First post',
+            content: 'This is the first post!',
+            imageUrl: 'images/boat.png',
+            creator: {
+                name: 'Nedim'
+            },
+            createdAt: new Date()
+        }]
     });
 };
 
 exports.createPost = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({
+            message: 'Validation failed, entered data is incorrect.',
+            errors: errors.array()
+        });
+    }
     const title = req.body.title;
     const content = req.body.content;
-    res.status(201).json({
-        message: 'post created successfully!',
-        post: {id: Date.now(), title, content: content}
+    const post = new Post({
+        title: title,
+        content: content,
+        imageUrl: 'images/boat.png',
+        creator: {
+            name: 'Nedim'
+        },
     });
+    post.save()
+        .then(result => {
+            console.log(result);
+            res.status(201).json({
+                message: 'post created successfully!',
+                post: result
+            });
+        })
+        .catch(err => console.log(err));
 };
