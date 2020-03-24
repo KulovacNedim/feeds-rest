8b2d5206cde825643d91d43ac78b7e2579dc91e8
Nedim Kulovac <nedim.kulovac@gmail.com>
2020-03-24 12:19:25 +0100

    multer installed and file upload on createPost function in feed controller implemented

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,36 +1,43 @@
 const {validationResult} = require('express-validator/check');
+const path = require('path');
 
-const Post = require('../models/post');
+const Post = require(path.join(__dirname, '../models/post'));
 
 exports.getPosts = (req, res, next) => {
-    res.status(200).json({
-        posts: [{
-            _id: '1',
-            title: 'First post',
-            content: 'This is the first post!',
-            imageUrl: 'images/boat.png',
-            creator: {
-                name: 'Nedim'
-            },
-            createdAt: new Date()
-        }]
-    });
+    Post.find()
+        .then(posts => {
+            res.status(200).json({
+                message: 'Fetched posts successfully.',
+                posts: posts
+            });
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        });
 };
 
 exports.createPost = (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(422).json({
-            message: 'Validation failed, entered data is incorrect.',
-            errors: errors.array()
-        });
+        const error = new Error('Validation failed, entered data is incorrect.');
+        error.statusCode = 422;
+        throw error;
     }
+    if (!req.file) {
+        const error = new Error('No image provided.');
+        error.statusCode = 422;
+        throw error;
+    }
+    const imageUrl = req.file.path;
     const title = req.body.title;
     const content = req.body.content;
     const post = new Post({
         title: title,
         content: content,
-        imageUrl: 'images/boat.png',
+        imageUrl: imageUrl,
         creator: {
             name: 'Nedim'
         },
@@ -43,5 +50,32 @@ exports.createPost = (req, res, next) => {
                 post: result
             });
         })
-        .catch(err => console.log(err));
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        });
+};
+
+exports.getPost = (req, res, next) => {
+    const postId = req.params.postId;
+    Post.findById(postId)
+        .then(post => {
+            if (!post) {
+                const error = new Error('Could not find post');
+                error.statusCode = 404;
+                throw error;
+            }
+            res.status(200).json({
+                message: 'Post fetched',
+                post: post
+            });
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.status = 500;
+            }
+            next(err);
+        });
 };
