37a0d244c4de261fb661be580d48336e75937696
Nedim Kulovac <nedim.kulovac@gmail.com>
2020-03-24 22:12:42 +0100

    socket.io installed and socket implemented to update addition, updating and delation of posts in frontend

--- controllers/feed.js
+++ controllers/feed.js
@@ -2,6 +2,7 @@ const {validationResult} = require('express-validator/check');
 const path = require('path');
 const fs = require('fs');
 
+const io = require('../socket');
 const Post = require(path.join(__dirname, '../models/post'));
 const User = require(path.join(__dirname, '../models/user'));
 
@@ -11,6 +12,8 @@ exports.getPosts = async (req, res, next) => {
     try {
         const totalItems = await Post.find().countDocuments();
         const posts = await Post.find()
+            .populate('creator')
+            .sort({createdAt: -1})
             .skip((currentPage - 1) * perPage)
             .limit(perPage);
 
@@ -53,6 +56,7 @@ exports.createPost = async (req, res, next) => {
         const user = await User.findById(req.userId);
         user.posts.push(post);
         await user.save();
+        io.getIO().emit('posts', {action: 'create', post: {...post._doc, creator: {_id: req.userId, name: user.name}}});
         res.status(201).json({
             message: 'Post created successfully!',
             post: post,
@@ -104,13 +108,13 @@ exports.updatePost = async (req, res, next) => {
         throw error;
     }
     try {
-        const post = await Post.findById(postId);
+        const post = await Post.findById(postId).populate('creator');
         if (!post) {
             const error = new Error('Could not find post.');
             error.statusCode = 404;
             throw error;
         }
-        if (post.creator.toString() !== req.userId) {
+        if (post.creator._id.toString() !== req.userId) {
             const error = new Error('Not authorized!');
             error.statusCode = 403;
             throw error;
@@ -122,6 +126,7 @@ exports.updatePost = async (req, res, next) => {
         post.imageUrl = imageUrl;
         post.content = content;
         const result = await post.save();
+        io.getIO().emit({action: 'posts', post: result});
         res.status(200).json({ message: 'Post updated!', post: result });
     } catch (err) {
         if (!err.statusCode) {
@@ -154,6 +159,7 @@ exports.deletePost = async (req, res, next) => {
         user.posts.pull(postId);
         await user.save();
 
+        io.getIO().emit('posts', {action: 'delete', post: postId});
         res.status(200).json({ message: 'Deleted post.' });
     } catch (err) {
         if (!err.statusCode) {
