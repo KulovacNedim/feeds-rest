f9511bc5198bb0a59d4927577e44234b16373f42
Nedim Kulovac <nedim.kulovac@gmail.com>
2020-04-07 23:14:02 +0200

    mongodb connection extracted to one enviromental variable so it can easier be change on heroku server to production db

--- app.js
+++ app.js
@@ -56,11 +56,11 @@ app.use((error, req, res, next) => {
         data: data
     });
 });
-
 mongoose.connect(
-    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-hhic1.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}?retryWrites=true&w=majority`
+    `${process.env.DEFAULT_MONGO_CONNECTION}`
 )
-    .then(result => {herokuDbNormalizer();
+    .then(result => {
+        herokuDbNormalizer();
         const server = app.listen(8080);
         const io = require('./socket').init(server);
         io.on('connection', socket => {
